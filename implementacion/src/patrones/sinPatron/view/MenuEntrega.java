2482f2f10675a0bbf22b5366e7fb8e1f50d49ab7
Cesar Carlier <ccarlier@espol.edu.ec>
2019-08-18 21:59:21 -0500

    Más fxml

--- implementacion/src/patrones/sinPatron/view/MenuEntrega.java
+++ implementacion/src/patrones/sinPatron/view/MenuEntrega.java
@@ -1,7 +1,17 @@
 package patrones.sinPatron.view;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MenuEntrega {
+    public Button botonRegresar;
+    public Label labelEntrega;
+
     public void consultarEntregas(){
         //TODO: Implementar
+    }
+
+    public void regresar(ActionEvent actionEvent) {
     }
 }
