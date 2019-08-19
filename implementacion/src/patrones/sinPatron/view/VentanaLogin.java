5f2993060a657ed0f089a5f0e2590b55d95875af
Cesar Carlier <ccarlier@espol.edu.ec>
2019-08-18 21:01:10 -0500

    Implementacion FXML: Login

---
+++ implementacion/src/patrones/sinPatron/view/VentanaLogin.java
@@ -0,0 +1,22 @@
+package patrones.sinPatron.view;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class VentanaLogin {
+    public Button BotonSalir;
+    public Button BotonIngresar;
+    public PasswordField CampoContraseña;
+    public TextField CampoUsuario;
+    public Label labelNombreEmpresa;
+    public Label labelAdvertencia;
+
+    public void ingresar(ActionEvent actionEvent) {
+    }
+
+    public void cerrarSistema(ActionEvent actionEvent) {
+    }
+}
