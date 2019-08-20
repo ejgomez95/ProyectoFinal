e45d537c9ac70b59f259b0b2c3b3e34d743251a8
Cesar Carlier <ccarlier@espol.edu.ec>
2019-08-19 21:27:20 -0500

    Update MenuUsuario.java

--- implementacion/src/patrones/sinPatron/view/MenuUsuario.java
+++ implementacion/src/patrones/sinPatron/view/MenuUsuario.java
@@ -1,8 +1,15 @@
 package patrones.sinPatron.view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuUsuario {
     public Label labelMenu;
@@ -18,16 +25,36 @@ public class MenuUsuario {
         //TODO: Referir a interfaz TipoDeUsuario
     }
 
-    public void lanzarMenuVentas(ActionEvent actionEvent) {
+    public void lanzarMenuVentas(ActionEvent actionEvent) throws IOException {
+        Parent dashboardParent = FXMLLoader.load(getClass().getResource("MenuVentas.fxml"));
+        Scene dashboardScene = new Scene(dashboardParent);
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
     }
 
-    public void lanzarMenuEntregas(ActionEvent actionEvent) {
+    public void lanzarMenuEntregas(ActionEvent actionEvent) throws IOException {
+        Parent dashboardParent = FXMLLoader.load(getClass().getResource("MenuEntrega.fxml"));
+        Scene dashboardScene = new Scene(dashboardParent);
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
     }
 
-    public void lanzarMenuLocal(ActionEvent actionEvent) {
+    public void lanzarMenuLocal(ActionEvent actionEvent) throws IOException {
+        Parent dashboardParent = FXMLLoader.load(getClass().getResource("MenuLocal.fxml"));
+        Scene dashboardScene = new Scene(dashboardParent);
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
     }
 
-    public void lanzarMenuEmpresa(ActionEvent actionEvent) {
+    public void lanzarMenuEmpresa(ActionEvent actionEvent) throws IOException {
+        Parent dashboardParent = FXMLLoader.load(getClass().getResource("MenuEmpresa.fxml"));
+        Scene dashboardScene = new Scene(dashboardParent);
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
     }
 
     public void salirSistema(ActionEvent actionEvent) {
