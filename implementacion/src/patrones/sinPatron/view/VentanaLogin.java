34c3689c1bd7d292e06527749127d1c1088a3b79
Cesar Carlier <ccarlier@espol.edu.ec>
2019-08-19 21:20:57 -0500

    Update VentanaLogin.java

--- implementacion/src/patrones/sinPatron/view/VentanaLogin.java
+++ implementacion/src/patrones/sinPatron/view/VentanaLogin.java
@@ -1,10 +1,17 @@
 package patrones.sinPatron.view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class VentanaLogin {
     public Button BotonSalir;
@@ -14,7 +21,13 @@ public class VentanaLogin {
     public Label labelNombreEmpresa;
     public Label labelAdvertencia;
 
-    public void ingresar(ActionEvent actionEvent) {
+    public void ingresar(ActionEvent actionEvent) throws IOException {
+        //Codigo de lanzamiento para la ventana del menuUsuario
+        Parent dashboardParent = FXMLLoader.load(getClass().getResource("MenuUsuario.fxml"));
+        Scene dashboardScene = new Scene(dashboardParent);
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
     }
 
     public void cerrarSistema(ActionEvent actionEvent) {
